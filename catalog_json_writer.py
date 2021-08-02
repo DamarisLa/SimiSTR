49ab18398abd550ee4c7fe9342cb673dd86b5576
DamarisLa <S1810458011@students.fh-hagenberg.at>
2021-08-03 00:26:00 +0200

    bedfile to json writer added intentations

--- catalog_json_writer.py
+++ catalog_json_writer.py
@@ -11,15 +11,16 @@ def catalogJsonWriter (bedfile, jsonfile):
     bf_d = dict()
     for line in bf:
         print(line)
+        locusid = str(line[0].strip())+"_"+str(line[1].strip())+"_"+str(line[-1].strip())
         referenceRegion = str(line[0])+":"+str(line[1])+"-"+str(line[2])
-        str_string = '{"LocusID": "'+ str(line[-1]).strip() + '", "LocusStructure": "('+ line[-1].strip() + ')*", '\
+        str_string = '{"LocusId": "'+ locusid + '", "LocusStructure": "('+ line[-1].strip() + ')*", '\
                      + '"ReferenceRegion": "' + referenceRegion + '", '\
                      +'"VariantType": "Repeat"}'
         bf_l.append(json.loads(str_string))
     with open(jsonfile, 'w') as json_file:
         #for i in bf_l:
         #    json.dump(i, json_file)
-        json.dump(bf_l,json_file)
+        json_file.write(json.dumps(bf_l,indent = 4, sort_keys=False))
 
 
 
